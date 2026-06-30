2d657ad306a080479e536cac0961bd99f2f932d6
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-06-30 18:17:32 -0400

    temporarily disable nfs mnt

--- backuper/default.nix
+++ backuper/default.nix
@@ -20,20 +20,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    fileSystems."/mnt/nfs" = {
-      device = "/dev/disk/by-uuid/6ee83677-4096-4a6f-8795-dc23a16dfb4b";
-      fsType = "ext4";
-    };
-
-    services.openssh = {
-      enable = true;
-      ports = [ 22 ];
-      settings = {
-        PasswordAuthentication = false;
-        AllowUsers = [ "joris" ];
-        PermitRootLogin = "no";
-      };
-    };
+    # fileSystems."/mnt/nfs" = {
+    #   device = "/dev/disk/by-uuid/6ee83677-4096-4a6f-8795-dc23a16dfb4b";
+    #   fsType = "ext4";
+    # };
 
     users.users.joris.packages = [
       (pkgs.writeShellScriptBin
